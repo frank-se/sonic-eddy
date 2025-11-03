0be049720c8a3bd5aa33e355aa60412a0873bde4
Frank Krick <frank.krick@gmail.com>
2025-11-03 18:59:53 -0500

    Build object browser tree

--- SonicEddy/ViewModels/ObjectBrowserViewModel.cs
+++ SonicEddy/ViewModels/ObjectBrowserViewModel.cs
@@ -19,7 +19,7 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
     public ObjectBrowserViewModel()
     {
         var clients = Wireplumber.Clients;
-        Objects = new(clients.Select(PipewireObject.FromClient));
+        Objects = new ObservableCollection<PipewireObject>(clients.Select(PipewireObject.FromClient));
 
         ProcessDevices();
         ProcessNodes();
@@ -76,43 +76,19 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
 
         if (_leftover.Count <= 0) return;
 
-        var potentialChanges = new List<PipewireObject>();
+        var potentiallyChangedDevices = Wireplumber.Devices.Where(d =>
+            d.Client.Id == client.ObjectId && _leftover.Any(l => l.ObjectSerial == d.ObjectSerial)).ToList();
 
-        foreach (var leftover in _leftover.ToList())
-            if (leftover.Type == PipewireObjectType.Device)
-            {
-                var device = Wireplumber.Devices.First(d =>
-                    d.ObjectSerial == leftover.ObjectSerial);
+        _leftover.RemoveAll(l => potentiallyChangedDevices.Any(d => d.ObjectSerial == l.ObjectSerial));
+        foreach (var device in potentiallyChangedDevices) HandleDeviceAddedEvent(device);
 
-                if (device.Client.Id != client.ObjectId) continue;
+        var potentiallyChangedNodes = Wireplumber.Nodes.Where(n => n.Client != null &&
+                                                                   n.Client.Id == client.ObjectId &&
+                                                                   _leftover.Any(l => l.ObjectSerial == n.ObjectSerial))
+            .ToList();
 
-                potentialChanges.Add(leftover);
-                _leftover.Remove(leftover);
-            }
-            else if (leftover.Type == PipewireObjectType.Node)
-            {
-                var node = Wireplumber.Nodes.First(n =>
-                    n.ObjectSerial == leftover.ObjectSerial);
-
-                if (node.Client?.Id != client.ObjectId) continue;
-
-                potentialChanges.Add(leftover);
-                _leftover.Remove(leftover);
-            }
-
-        foreach (var potentialChange in potentialChanges)
-            if (potentialChange.Type == PipewireObjectType.Device)
-            {
-                var device = Wireplumber.Devices.First(d =>
-                    d.ObjectSerial == potentialChange.ObjectSerial);
-                HandleDeviceAddedEvent(device);
-            }
-            else if (potentialChange.Type == PipewireObjectType.Node)
-            {
-                var node = Wireplumber.Nodes.First(n =>
-                    n.ObjectSerial == potentialChange.ObjectSerial);
-                HandleNodeAddedEvent(node);
-            }
+        _leftover.RemoveAll(l => potentiallyChangedNodes.Any(n => n.ObjectSerial == l.ObjectSerial));
+        foreach (var node in potentiallyChangedNodes) HandleNodeAddedEvent(node);
     }
 
     private void HandleDeviceAddedEvent(Device device)
@@ -139,24 +115,10 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
 
         if (_leftover.Count <= 0) return;
 
-        var potentialChanges = new List<PipewireObject>();
-        var nodes =
-            Wireplumber.Nodes.Where(n => n.Device?.Id == device.ObjectId)
-                .ToList();
-
-        foreach (var nodeObject in nodes.Select(node =>
-                         Objects.FirstOrDefault(n =>
-                             n.Type == PipewireObjectType.Node &&
-                             n.ObjectId == node.ObjectId))
-                     .OfType<PipewireObject>())
-        {
-            _leftover.Remove(nodeObject);
-            potentialChanges.Add(nodeObject);
-        }
-
-        foreach (var potentialChange in potentialChanges)
-            HandleNodeAddedEvent(nodes.First(n =>
-                n.ObjectId == potentialChange.ObjectId));
+        var potentiallyChangedNodes = Wireplumber.Nodes.Where(n =>
+            n.Device?.Id == device.ObjectId && _leftover.Any(l => l.ObjectSerial == n.ObjectSerial)).ToList();
+        _leftover.RemoveAll(l => potentiallyChangedNodes.Any(n => n.ObjectSerial == l.ObjectSerial));
+        foreach (var node in potentiallyChangedNodes) HandleNodeAddedEvent(node);
     }
 
     private void HandleNodeAddedEvent(Node node)
@@ -170,6 +132,43 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
 
     private void AddNode(Node node)
     {
+        if (node.Client is null && node.Device is null)
+        {
+            Objects.Add(PipewireObject.FromNode(node));
+        }
+        else if (node.Device is null && node.Client != null)
+        {
+            var client = Objects.FirstOrDefault(o => o.ObjectId == node.Client.Id);
+            if (client is null)
+            {
+                _leftover.Add(PipewireObject.FromNode(node));
+                return;
+            }
+
+            client.Children.Add(PipewireObject.FromNode(node));
+        }
+        else if (node.Device != null && node.Client != null)
+        {
+            var client = Objects.FirstOrDefault(o => o.ObjectId == node.Client.Id);
+            if (client is null)
+            {
+                _leftover.Add(PipewireObject.FromNode(node));
+                return;
+            }
+
+            var device = client.Children.FirstOrDefault(d => d.ObjectId == node.Device.Id);
+            if (device is null)
+            {
+                _leftover.Add(PipewireObject.FromNode(node));
+                return;
+            }
+
+            device.Children.Add(PipewireObject.FromNode(node));
+        }
+
+        var potentiallyChangedPorts = Wireplumber.Ports.Where(p => p.Node.Id == node.ObjectId).ToList();
+        _leftover.RemoveAll(l => potentiallyChangedPorts.Any(p => p.ObjectSerial == l.ObjectSerial));
+        foreach (var port in potentiallyChangedPorts) HandlePortAddedEvent(port);
     }
 
     private void HandlePortAddedEvent(Port port)
@@ -183,58 +182,27 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
 
     private void AddPort(Port port)
     {
-    }
-
-    private void ProcessPorts()
-    {
-        var ports = Wireplumber.Ports;
         var nodes = Wireplumber.Nodes;
 
-        foreach (var port in ports)
+        var pipewireNode =
+            nodes.FirstOrDefault(n => n.ObjectId == port.Node.Id);
+        if (pipewireNode != null)
         {
-            var pipewireNode =
-                nodes.FirstOrDefault(n => n.ObjectId == port.Node.Id);
-            if (pipewireNode != null)
+            if (pipewireNode.Device != null)
             {
-                if (pipewireNode.Device != null)
+                var client = Objects.FirstOrDefault(c =>
+                    c.ObjectId == pipewireNode.Client!.Id);
+                if (client != null)
                 {
-                    var client = Objects.FirstOrDefault(c =>
-                        c.ObjectId == pipewireNode.Client!.Id);
-                    if (client != null)
+                    var device = client.Children.FirstOrDefault(d =>
+                        d.ObjectId == pipewireNode.Device.Id);
+                    if (device != null)
                     {
-                        var device = client.Children.FirstOrDefault(d =>
-                            d.ObjectId == pipewireNode.Device.Id);
-                        if (device != null)
-                        {
-                            var node = device.Children.FirstOrDefault(n =>
-                                n.ObjectId == port.Node.Id);
-                            if (node != null)
-                                node.Children.Add(
-                                    PipewireObject.FromPort(port));
-                            else
-                                _leftover.Add(PipewireObject.FromPort(port));
-                        }
-                        else
-                        {
-                            _leftover.Add(PipewireObject.FromPort(port));
-                        }
-                    }
-                    else
-                    {
-                        _leftover.Add(PipewireObject.FromPort(port));
-                    }
-                }
-                else if (pipewireNode.Client != null)
-                {
-                    var client = Objects.FirstOrDefault(c =>
-                        c.ObjectId == pipewireNode.Client.Id);
-                    if (client != null)
-                    {
-                        var node =
-                            client.Children.FirstOrDefault(n =>
-                                n.ObjectId == port.Node.Id);
+                        var node = device.Children.FirstOrDefault(n =>
+                            n.ObjectId == port.Node.Id);
                         if (node != null)
-                            node.Children.Add(PipewireObject.FromPort(port));
+                            node.Children.Add(
+                                PipewireObject.FromPort(port));
                         else
                             _leftover.Add(PipewireObject.FromPort(port));
                     }
@@ -245,19 +213,48 @@ public class ObjectBrowserViewModel : ViewModelBase, IActivatableViewModel
                 }
                 else
                 {
+                    _leftover.Add(PipewireObject.FromPort(port));
+                }
+            }
+            else if (pipewireNode.Client != null)
+            {
+                var client = Objects.FirstOrDefault(c =>
+                    c.ObjectId == pipewireNode.Client.Id);
+                if (client != null)
+                {
                     var node =
-                        Objects.FirstOrDefault(n => n.ObjectId == port.Node.Id);
+                        client.Children.FirstOrDefault(n =>
+                            n.ObjectId == port.Node.Id);
                     if (node != null)
                         node.Children.Add(PipewireObject.FromPort(port));
                     else
                         _leftover.Add(PipewireObject.FromPort(port));
                 }
+                else
+                {
+                    _leftover.Add(PipewireObject.FromPort(port));
+                }
             }
             else
             {
-                _leftover.Add(PipewireObject.FromPort(port));
+                var node =
+                    Objects.FirstOrDefault(n => n.ObjectId == port.Node.Id);
+                if (node != null)
+                    node.Children.Add(PipewireObject.FromPort(port));
+                else
+                    _leftover.Add(PipewireObject.FromPort(port));
             }
         }
+        else
+        {
+            _leftover.Add(PipewireObject.FromPort(port));
+        }
+    }
+
+    private void ProcessPorts()
+    {
+        var ports = Wireplumber.Ports;
+        foreach (var port in ports) AddPort(port);
     }
 
     private void ProcessNodes()
