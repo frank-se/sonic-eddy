67018b03b0ae3a2cf70101a74508726533f5add4
Frank Krick <frank.krick@gmail.com>
2026-06-11 16:12:20 -0400

    c log level depends on buildtype

--- fr-sonic/src/logging/log.h
+++ fr-sonic/src/logging/log.h
@@ -8,7 +8,10 @@ namespace logging {
 
 enum class LogLevel { None = 0, Fatal, Error, Warning, Info, Debug, Trace };
 
-inline constexpr auto LOG_LEVEL = LogLevel::Trace;
+#ifndef FRSONIC_LOG_LEVEL
+#define FRSONIC_LOG_LEVEL 6
+#endif
+inline constexpr auto LOG_LEVEL = static_cast<LogLevel>(FRSONIC_LOG_LEVEL);
 
 constexpr std::string_view to_string(const LogLevel level) noexcept {
   switch (level) {
